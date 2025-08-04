10ece9144f8a84328e43e4626ea57f9106840592
sudeepa99 <sudeepa1234nisal@gmail.com>
2025-08-04 20:03:32 +0530

    app.js defined with dummy routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { AuthProvider } from "./contexts/authContext";
+import { Route, Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 const theme = createTheme({
   pallete: {
     primary: {
@@ -20,6 +23,34 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            {/* <Route path="/login" element={<Login/>}/> */}
+            {/* <Route path="/register" element={<Register />} /> */}
+
+            {/* <Route path="/" element={<PrivateRoute />}>
+            <Route index element={<Home />} />
+              <Route path="books" element={<Books />} />
+              <Route path="books/:id" element={<BookDetails />} />
+              <Route path="my-borrowings" element={<MyBorrowings />} />
+              
+              <Route path="admin" element={<AdminRoute />}>
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="books/add" element={<AddBook />} />
+                <Route path="books/edit/:id" element={<EditBook />} />
+                <Route path="users" element={<Users />} />
+                <Route path="users/:id" element={<UserDetails />} />
+                <Route path="borrowings" element={<Borrowings />} />
+                <Route path="overdue-books" element={<OverdueBooks />} />
+              </Route>
+
+            </Route> */}
+            {/* <Route path="*" element={<NotFound />} /> */}
+          </Routes>
+        </Router>
+      </AuthProvider>
+      <ToastContainer position="top-right" autoClose={5000} />
     </ThemeProvider>
   );
 }

d0eb46f016e11b5f65bedbf4c33d92867fab6727
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-01-23 21:44:31 +0530

    created loader component and used it in necessary places

---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,9 @@
+const PageLoader = () => {
+  return (
+    <div className="flex h-[60vh] items-center justify-center">
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-blue-600" />
+    </div>
+  );
+};
+
+export default PageLoader;

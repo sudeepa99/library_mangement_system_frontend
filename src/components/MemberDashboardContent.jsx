29695a8284a2126ea325b9b4b1680170a9869b21
sudeepa99 <sudeepa1234nisal@gmail.com>
2026-02-18 07:50:36 +0530

    changed the scrolling and scroll bar of books catalogue view

--- src/components/MemberDashboardContent.jsx
+++ src/components/MemberDashboardContent.jsx
@@ -84,70 +84,75 @@ const MemberDashboardContent = () => {
   if (loading) return <PageLoader />;
 
   return (
-    <div className="px-[4%] py-[2%]">
-      <h2 className="text-2xl font-bold text-gray-800 mb-6">Books Catalogue</h2>
+    <div className="h-full flex flex-col px-[4%] py-[2%]">
+      <div className="flex-shrink-0">
+        <h2 className="text-2xl font-bold text-gray-800 mb-6">
+          Books Catalogue
+        </h2>
+      </div>
+      <div className="flex-1 px-4 overflow-y-auto  scrollbar-thin scrollbar-thumb-[#8C92AC] scrollbar-track-gray-200 hover:scrollbar-thumb-[#00843f]   ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {books.map((book) => {
+            const isAvailable = book.availableCopies > 0;
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        {books.map((book) => {
-          const isAvailable = book.availableCopies > 0;
-
-          return (
-            <div
-              key={book._id}
-              className="bg-white rounded-xl shadow-sm flex flex-col p-5  border-gray-400 border-2  transition-all duration-300 ease-out
-            hover:scale-103 hover:-translate-y-1 hover:shadow-xl"
-            >
-              <div className="flex justify-center mb-4">
-                <div className="h-16 w-16 rounded-full bg-blue-100 flex items-center justify-center text-xl font-bold text-blue-600">
-                  {book.title.charAt(0).toUpperCase()}
+            return (
+              <div
+                key={book._id}
+                className="bg-white rounded-xl shadow-sm flex flex-col p-5 border-gray-400 border-2 transition-all duration-300 ease-out
+              hover:scale-103 hover:-translate-y-1 hover:shadow-xl"
+              >
+                <div className="flex justify-center mb-4">
+                  <div className="h-16 w-16 rounded-full bg-blue-100 flex items-center justify-center text-xl font-bold text-blue-600">
+                    {book.title.charAt(0).toUpperCase()}
+                  </div>
                 </div>
-              </div>
 
-              <div className="flex-grow text-center">
-                <h3 className="text-base font-semibold text-gray-800 line-clamp-2">
-                  {book.title}
-                </h3>
-                <p className="text-sm text-gray-500 mt-1 line-clamp-1">
-                  {book.author}
-                </p>
+                <div className="flex-grow text-center">
+                  <h3 className="text-base font-semibold text-gray-800 line-clamp-2">
+                    {book.title}
+                  </h3>
+                  <p className="text-sm text-gray-500 mt-1 line-clamp-1">
+                    {book.author}
+                  </p>
 
-                <div className="mt-3">
-                  {isAvailable ? (
-                    <span className="inline-block px-3 py-1 text-xs rounded-full bg-green-100 text-green-700 font-medium">
-                      Available
-                    </span>
-                  ) : (
-                    <span className="inline-block px-3 py-1 text-xs rounded-full bg-red-100 text-red-700 font-medium">
-                      Not Available
-                    </span>
-                  )}
+                  <div className="mt-3">
+                    {isAvailable ? (
+                      <span className="inline-block px-3 py-1 text-xs rounded-full bg-green-100 text-green-700 font-medium">
+                        Available
+                      </span>
+                    ) : (
+                      <span className="inline-block px-3 py-1 text-xs rounded-full bg-red-100 text-red-700 font-medium">
+                        Not Available
+                      </span>
+                    )}
+                  </div>
                 </div>
-              </div>
 
-              <div className="mt-5 flex flex-col gap-2">
-                <button
-                  disabled={!isAvailable}
-                  onClick={() => createBorrowing(book._id)}
-                  className={`w-full py-2 rounded-md font-medium transition
+                <div className="mt-5 flex flex-col gap-2">
+                  <button
+                    disabled={!isAvailable}
+                    onClick={() => createBorrowing(book._id)}
+                    className={`w-full py-2 rounded-md font-medium transition
                     ${
                       isAvailable
                         ? "bg-[#009B4D] text-white hover:bg-[#00843f]"
                         : "bg-gray-200 text-gray-400 cursor-not-allowed"
                     }`}
-                >
-                  Borrow
-                </button>
+                  >
+                    Borrow
+                  </button>
 
-                <button
-                  className="w-full py-2 rounded-md border border-gray-300 text-sm hover:bg-gray-300"
-                  onClick={() => window.open(book.moreInfoLink, "_blank")}
-                >
-                  See More
-                </button>
+                  <button
+                    className="w-full py-2 rounded-md border border-gray-300 text-sm hover:bg-gray-300"
+                    onClick={() => window.open(book.moreInfoLink, "_blank")}
+                  >
+                    See More
+                  </button>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
